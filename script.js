f2e64d73ff5f3365e2b6389b3a4f44d9ec57d0a4
ALECLASHER <139481002+ALECLASHER@users.noreply.github.com>
2023-07-27 13:47:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 
-  if ((username === 'just_gine17' || username === 'Just_gine17') && password === 'ciao38183133') {
+  if ((username === 'just_gine17' || username === 'Just_gine17') && password === '27072123') {
     window.location.href = 'welcome.html';
   } else {
     document.getElementById('error-message').textContent = 'apprezzo il tentativo, capirai.';
