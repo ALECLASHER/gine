97f8f4848a5eca408f8157c1a0e308bfba913ca5
ALECLASHER <139481002+ALECLASHER@users.noreply.github.com>
2024-04-07 01:34:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document.getElementById('login-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-
-  if ((username === 'just_gine17' || username === 'Just_gine17') && password === '27072123') {
-    window.location.href = 'welcome.html';
-  } else {
-    document.getElementById('error-message').textContent = 'apprezzo il tentativo, capirai.';
-  }
-});
-
 document.getElementById("days-button").addEventListener("click", function () {
   window.location.href = "giorni.html";
 });
